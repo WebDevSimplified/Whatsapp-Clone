aa535cbc788a7fdfa2042dcdd7b66df9a3683e34
noob <shelcia1999@gmail.com>
2020-10-07 03:38:59 +0530

    bug fix

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,22 +1,28 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-const PREFIX = 'whatsapp-clone-'
+const PREFIX = "whatsapp-clone-";
 
 export default function useLocalStorage(key, initialValue) {
-  const prefixedKey = PREFIX + key
+  const prefixedKey = PREFIX + key;
   const [value, setValue] = useState(() => {
-    const jsonValue = localStorage.getItem(prefixedKey)
-    if (jsonValue != null) return JSON.parse(jsonValue)
-    if (typeof initialValue === 'function') {
-      return initialValue()
-    } else {
-      return initialValue
+    const jsonValue = localStorage.getItem(prefixedKey);
+    if (jsonValue != null) {
+      if (jsonValue === "undefined") {
+        return null;
+      } else {
+        return JSON.parse(jsonValue);
+      }
     }
-  })
+    if (typeof initialValue === "function") {
+      return initialValue();
+    } else {
+      return initialValue;
+    }
+  });
 
   useEffect(() => {
-    localStorage.setItem(prefixedKey, JSON.stringify(value))
-  }, [prefixedKey, value])
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
 
-  return [value, setValue]
+  return [value, setValue];
 }
